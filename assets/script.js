6f1d521f4b519dbc01d40e7b859b266c220dcff3
Gaines Yeilding <gfy613@gmail.com>
2020-06-20 19:56:05 -0400

    fixed images in portfolio

--- assets/script.js
+++ assets/script.js
@@ -5,37 +5,37 @@ const codingProjects = [
      projectDescription: "Was given a semi-complete HTML and was asked to correct issues invloving syntax and general clean up of code",
      githubRepo: "https://github.com/gfy613/Homework_1_Code_-Refactor",
      githubSite: "https://gfy613.github.io/Homework_1_Code_-Refactor/",
-     images: "assets/images/code_refactor.jpg"
+     images: "assets/images/code_refactor.JPG"
     },
     {projectName: "Password Generator",
      projectDescription: "Generates a password between 8-126 characters. User can choose to use upper and lower case letters, numbers, or special characters.",
      githubRepo: "https://github.com/gfy613/password_generator",
      githubSite: "https://gfy613.github.io/password_generator/",
-     images: "assets/images/password_generator.jpg"
+     images: "assets/images/password_generator.JPG"
     },
     {projectName: "Code Quiz",
      projectDescription: "Created a timed quiz that cycles through a series of questions about javascript. If the user gets a question correct it increases the score and if they get it wrong they loose 10 seconds. Uses local storage to record and show past highscores.",
      githubRepo: "https://github.com/gfy613/code_quiz",
      githubSite: "https://gfy613.github.io/code_quiz/",
-     images: "assets/images/code_quiz.jpg"
+     images: "assets/images/code_quiz.JPG"
     },
     {projectName: "Day Planner",
      projectDescription: "Creates a day planner for a user to keep track of their day. Saves tasks to local storage and formats each hour based on the time of day.",
      githubRepo: "https://github.com/gfy613/day_planner",
      githubSite: "https://gfy613.github.io/day_planner/",
-     images: "assets/images/day_planner.jpg"
+     images: "assets/images/day_planner.JPG"
     },
     {projectName: "Weather Dashboard",
      projectDescription: "Created a weather app using an API. The user can see the current weather and 5 day forecast based on a city they choose.",
      githubRepo: "https://github.com/gfy613/weather_forecast",
      githubSite: "https://gfy613.github.io/weather_forecast/",
-     images: "assets/images/weather_dashboard.jpg"
+     images: "assets/images/weather_dashboard.JPG"
     },
     {projectName: "Fitness Tracker",
      projectDescription: "Group project where we created a fitness tracker. In this project we used APIs to generate a workout based on the user inputs. It would also generate a daily calorie intake based on the users weight goal. From there it would track the users weight and create a graph of their weight loss/gain. It would also display the daily weather and show a weekly task list.",
      githubRepo: "https://github.com/evandev01/fitness-tracker",
      githubSite: "https://evandev01.github.io/fitness-tracker/",
-     images: "assets/images/fitness_tracker.jpg"
+     images: "assets/images/fitness_tracker.JPG"
     }
 ]
 
